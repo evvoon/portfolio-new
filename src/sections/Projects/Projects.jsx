7c270da2f43004467e8b53d46365bceab3825c43
Mona Lisa <joorysalam786@gmail.com>
2024-08-08 18:05:23 +0400

    Updates

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,49 @@
+import styles from "./ProjectsStyles.module.css";
+import chess from "../../assets/chess.png";
+import stream from "../../assets/stream-app.png";
+import maps from "../../assets/google-maps-clone.png";
+import race from "../../assets/race-ball-game.png";
+import stopwatch from "../../assets/stopwatch.png";
+import ProjectCard from "../../common/ProjectCard";
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={chess}
+          link={"https://chess-evvoon.vercel.app/"}
+          h3="Chess"
+          p="Built with Reactjs"
+        />
+        <ProjectCard
+          src={stream}
+          link={"https://github.com/evvoon/chat-application"}
+          h3="Chat Application"
+          p="Built with stream and Reactjs"
+        />
+        <ProjectCard
+          src={maps}
+          link={"https://google-maps-clone-topaz.vercel.app/"}
+          h3="Google Maps Clone"
+          p="Made with Mapbox API"
+        />
+        <ProjectCard
+          src={race}
+          link={"https://r3f-race-game.vercel.app/"}
+          h3="Race Ball Game"
+          p="made with Threejs"
+        />
+        <ProjectCard
+          src={stopwatch}
+          link={"https://stopwatch-demo.vercel.app/"}
+          h3="Stopwatch"
+          p="Very first project :)"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
---
+++ src/sections/Skills/Skills.jsx
@@ -0,0 +1,27 @@
+import styles from "./SkillsStyles.module.css";
+import checkMarkIcon from "../../assets/checkmark-dark.svg";
+import SkillList from "../../common/SkillList";
+
+function Skills() {
+  return (
+    <section id="skills" className={styles.container}>
+      <h1 className="sectionTitle">Skills</h1>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="HTML" />
+        <SkillList src={checkMarkIcon} skill="CSS" />
+        <SkillList src={checkMarkIcon} skill="Javascript" />
+        <SkillList src={checkMarkIcon} skill="Typescript" />
+        <SkillList src={checkMarkIcon} skill="Node" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkMarkIcon} skill="Vue" />
+        <SkillList src={checkMarkIcon} skill="Tailwind" />
+        <SkillList src={checkMarkIcon} skill="Git" />
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
